042f2e7702845f762c6c8451337db576abd4c898
Somies <misaelsoria789@gmail.com>
2025-03-16 13:34:01 -0500

    guardar cambio de nombre

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com"], // ✅ Permite imágenes de Cloudinary
+    domains: ["res.cloudinary.com", "lh3.googleusercontent.com"], // ✅ Agregamos Google
   },
 };
 
