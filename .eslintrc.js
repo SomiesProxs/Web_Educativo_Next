aff6816c518ca160bff145add4d6e99719f92b6b
Somies <misaelsoria789@gmail.com>
2025-05-03 21:41:03 -0500

    extraer

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-    extends: 'next/core-web-vitals',
-    rules: {
-      // Desactiva completamente la regla de variables no utilizadas
-      '@typescript-eslint/no-unused-vars': 'off',
-      'react-hooks/exhaustive-deps': 'warn'
-    }
-  };
+  extends: 'next/core-web-vitals',
+  rules: {
+    // Desactiva completamente la regla de variables no utilizadas
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react-hooks/exhaustive-deps': 'warn'
+  }
+};
